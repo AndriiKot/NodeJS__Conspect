003eccb0ff741ec73ad93fc862f34d2f4b83085f
AndriiKot <a_kocyuba@ukr.net>
2024-09-01 19:02:00 +0200

    __NodeJS__conspect ! CREATE NEW CONSPECT NODEJS   !

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# Node Conspect
+
---
+++ __01__COOKS__/__001-01__createServer.js
@@ -0,0 +1,13 @@
+﻿const http = require('node:http');
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain"});
+  res.end("Hello World!!!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () => 
+  console.log(`Server started on port ${port}; press Ctrl-C to terminate....`)
+);
