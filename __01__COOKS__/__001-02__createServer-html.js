d2b23e87f4bc8d399f28263d7d917eb734efff42
AndriiKot <a_kocyuba@ukr.net>
2024-09-02 09:24:33 +0200

    __NodeJS__conspect ! create Server html cooks  !

---
+++ __01__COOKS__/__001-02__createServer-html.js
@@ -0,0 +1,13 @@
+﻿const http = require("node:http");
+const port = process.env.PORT || 3000;
+
+const requestHandler = (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello world!</h1>");                       
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () =>
+  console.log(`Server started on port ${port}; press Ctrl-C to terminate....`)
+);
