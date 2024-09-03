7d13d1120fa0f0958229bd80372cc29ce0efe9a4
AndriiKot <a_kocyuba@ukr.net>
2024-09-03 09:56:45 +0200

    __NodeJS__conspect ! routing object  !

---
+++ __01__COOKS__/__002-02__routing-object.js
@@ -0,0 +1,28 @@
+﻿const http = require("node:http");
+const port = process.env.PORT || 3000;
+
+const routes = {
+  "": (res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Homepage");
+  },
+  "/about": (res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("About");
+  },
+};
+
+const requestHandler = (req, res) => {
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+  const routeHandler = routes[path] || (res) => {
+    
+  }
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, () =>
+  console.log(`Server started on port ${port}; press Ctrl-C to terminate....`)
+);
