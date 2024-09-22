60fcd851be74bc3c7cf7ed0d82a86e264e4da20e
AndriiKot <a_kocyuba@ukr.net>
2024-09-22 10:13:50 +0200

    __NodeJS__conspect !  API URLSearchParams()  !

---
+++ __00__CONSPECT__/url/URLSearchParams/__00-0__new-URLSearchParams__.js
@@ -0,0 +1,33 @@
+﻿const http = require("node:http");
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    const body = [];
+
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    req.on("data", (chunk) => {
+      body.push(Buffer.from(chunk)); // Append chunk.toString();
+    });
+    req.on("end", () => {
+      const data = new URLSearchParams(body.toString());  // !!! new URLSearchParams() !!!
+      console.log(data.get("title"));
+      res.end(`<h1>Form data submitted ФФФФФФФ!</h1>`);
+    });
+  } else if (req.method === "GET") {
+    console.log(req.url);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <h1>Form</h1>
+      <form action="/" method="POST">
+        <input type="text" name="title">
+        <button type="submit">Send</button>
+      </form>
+    `);
+  }
+});
+
+server.listen(port, () =>
+  console.log(`Server started on port ${port}; press Ctrl-C to terminate....`)
+);
