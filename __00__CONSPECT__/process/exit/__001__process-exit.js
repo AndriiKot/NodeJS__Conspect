e1feae374b328acb5ca3333d17ceea2a16c9d09a
AndriiKot <a_kocyuba@ukr.net>
2024-09-28 23:14:00 +0200

    __NodeJS__conspect ! process.argv !

---
+++ __00__CONSPECT__/process/exit/__001__process-exit.js
@@ -0,0 +1,14 @@
+﻿const fs = require('node:fs');
+
+fs.readdir('data', (err, files) => {
+  if (err) {
+    console.error("Ошибка не могу прочитать файл data.");
+    process.exit(1);
+  }
+  const txtFiles = files.filter(f => /\.txt$/i.test(f));
+  if(!txtFiles.length) {
+    console.log("Файлы .txt не найдены.");
+    process.exit(0);
+  }
+});
+
