380b13f4112908bc0c4298b611c8bd05a06a6b5b
AndriiKot <a_kocyuba@ukr.net>
2024-09-28 22:25:46 +0200

    __NodeJS__conspect ! fs .readdir !

---
+++ __00__CONSPECT__/fs/readdir/ls.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+
+fs.readdir(__dirname, (err, files) => {
+  if (err) return console.error("Невозможно прочитать содержимое каталога");
+  console.log(`Содержимое каталога ${__dirname}:`);
+  console.log(files.map(f => '\t' + f).join('\n'));
+});
