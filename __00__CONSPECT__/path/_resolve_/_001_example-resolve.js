aaae840b87164cbcd5f15bda35075b36bc442752
AndriiKot <a_kocyuba@ukr.net>
2024-10-02 10:16:44 +0200

    __NodeJS__conspect ! path.resolve() and process.argv docs !

---
+++ __00__CONSPECT__/path/_resolve_/_001_example-resolve.js
@@ -0,0 +1,17 @@
+﻿const path = require('path');
+
+// Пример 1: Нормализация пути
+const normalizedPath = path.resolve('foo', 'bar', 'baz');
+console.log(normalizedPath); // Например, '/current/working/directory/foo/bar/baz'
+
+// Пример 2: Использование абсолютного пути
+const absolutePath = path.resolve('/foo', 'bar', 'baz');
+console.log(absolutePath); // Вывод: '/foo/bar/baz'
+
+// Пример 3: Использование относительных путей
+const relativePath = path.resolve('foo', '/bar', 'baz');
+console.log(relativePath); // Вывод: '/bar/baz' (игнорируя 'foo')
+
+// Пример 4: Путь с '..'
+const upPath = path.resolve('foo', '..', 'bar');
+console.log(upPath); // Выводит нормализованный путь, поднимающийся на уровень выше
---
+++ __00__CONSPECT__/process/argv/__002__process-argv-json.js
@@ -0,0 +1,15 @@
+﻿
+function consoleToJSON() {
+  const cons = {};
+  const argvs = process.argv
+  const len = argvs.length
+  
+  for(let i = 2; i < len; i++) {
+    const arg = argvs[i].split('=');
+    cons[arg[0]] = arg[1] ? arg[1] : true
+  }
+  
+  return cons
+}
+// node __10-01-0__process-argv-json.js message=hello spec=false
+console.log(consoleToJSON());  // { message: 'hello', spec: 'false' }
